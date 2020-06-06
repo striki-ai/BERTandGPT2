29b681428a77effda66cbbb3c4ef90b2214cb8cb
striki <stojancho.tudzarski@netcetera.com>
2020-06-06 19:01:06 +0200

    Few txt files deleted

--- ask-a-question/generate-domain-content.py
+++ ask-a-question/generate-domain-content.py
@@ -1,21 +1,26 @@
 from bs4 import BeautifulSoup
 import os
-# from langdetect import detect
 from langid.langid import LanguageIdentifier, model
-identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
+
+MIN_STRIP_CHARS = 16
+
+domain_content_paras = []
+
+lang_identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
 
 current_dir = os.path.abspath(os.getcwd())
 
-pars = []
-
+# generate list of all html files to be parsed
 html_files = []
-for root, _, files in os.walk(current_dir + '/html/'):
+for root, _, files in os.walk(current_dir + '\\html\\'):
     for file in files:
         if '.html' not in file:
             continue
         p=os.path.join(root,file)
         html_files.append(p)
 
+# Go through all of html files and extract text from them.
+# Add that text as set of paragraphs to domain_content file.
 for h in html_files:
     try:
         html_file = open(h,"r")
@@ -49,29 +54,22 @@ for h in html_files:
 
         strip = strip.replace('\', \' -', ' ')
         strip = strip.replace('\', \'', ' ')
-        strip = strip.replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
-        strip = strip.replace('&nbsp;', '')
+        strip = txt.replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
+        strip = txt.replace('&nbsp;', '')
 
-        if len(strip.split(' ')) > 4:
-            strips.append(strip)
-     
-    txt='\n'.join(strips)
-    txt = txt.replace('\', \' -', ' ')
-    txt = txt.replace('\', \'', ' ')
-    txt = txt.replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
-    txt = txt.replace('&nbsp;', '')
+        if len(strip) < MIN_STRIP_CHARS:
+            continue
+
+        strips.append(strip)
 
     try:
-        lang = identifier.classify(txt)[0]
-        # lang = detect(txt)
+        lang = lang_identifier.classify(' '.join(strips))[0]
         if lang != 'en':
             continue
     except:
         continue
 
-    for strip in strips:
-        if strip not in pars:
-            pars.append(strip)
+    domain_content_paras.extend(strips)
 
-with open("domain-content.txt", "w") as content:
-    content.write("\n\n".join(pars))
+with open("domain-content.txt", "w") as domain_content:
+    domain_content.write("\n\n".join(domain_content_paras))
