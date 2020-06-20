34cf356140da97d74b3b328066efc29c7b97bfd6
Stojancho Tudjarski <stojanco.tudzarski@netcetera.com>
2020-06-20 18:06:03 +0200

    data extraction from wiki xml finished.

--- clear_wiki.sh
+++ clear_wiki.sh
@@ -62,3 +62,23 @@ echo "step 31: remove lines with time only"
 sed -i 's/^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]Z$//' $1
 echo "step 32: remove lines with 31 random chars and numbers only"
 sed -i 's/^\w\{31\}$//' $1
+echo "step 33: remove text (including) !-- and --"
+sed -i 's/\!\-\-.*\?\-\-//g' $1
+echo "step 34: remove few tokens ..."
+sed -i 's/\|\(thumbnail\|thumb\|\d\d*px\|right\|center\|left\|padding\|border\|bg\|border2\|bg2\|upright\)//g' $1
+echo "step 35: remove |<digits>| (because sed doesn't understand \d)"
+sed -i 's/\|[0123456789][0123456789]*px\|//g' $1
+echo "step 36: remove multiple |s one by one"
+sed -i 's/|||*//g' $1
+echo "step 37: remove starting one or more : signs"
+sed -i 's/^::*//g' $1
+echo "step 38: remove two or more : signs"
+sed -i 's/:::*//g' $1
+echo "step 39: remove one or more space signs from the beginning of the lines"
+sed -i 's/^  *//g' $1
+echo "step 40: remove two or more space signs from the lines"
+sed -i 's/   *//g' $1
+echo "step 41: remove nowrap| from the lines"
+sed -i 's/[Nn]owrap|//g' $1
+echo "step 42: remove reflist from the beginning of the lines"
+sed -i 's/^[Rr]eflist.*//g' $1
